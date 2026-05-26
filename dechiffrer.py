7272e330de185eb91e3e204bcb398d9a4f2347b3
mshohoudi <mohammad.shohoudimojdehi.1@ens.etsmtl.ca>
2026-05-25 22:18:09 -0400

    mise à jour dechiffrer.py

--- dechiffrer.py
+++ dechiffrer.py
@@ -56,6 +56,23 @@ if __name__ == "__main__":
     print("Test déchiffrement :", decrypte)
     # Résultat attendu : veni, vidi, vici!
 #ALGORITHMES ENIGMA (Clé = Tuple de 3 entiers)
+def enigma_chiffrer(message: str, cle: tuple):
+    message_propre = normaliser_message(message)
+    resultat = ""
+    index_cle = 0
+
+    for char in message_propre:
+        if char in alphabet:
+            decalage = cle[index_cle % 3]
+            position_actuelle = alphabet.index(char)
+            nouvelle_position = (position_actuelle + decalage) % 26
+
+            resultat += alphabet[nouvelle_position]
+            index_cle += 1  # Incrémente seulement si c'est une lettre
+        else:
+            resultat += char
+
+    return resultat
 def enigma_dechiffrer(message: str, cle: tuple):
     message_propre = normaliser_message(message)
     resultat = ""
